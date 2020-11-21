bfac00e4e6893887dfdaff4c5079dda603e99508
tuanth9 <tuan16pm@gmail.com>
2020-11-21 13:55:40 +0700

    initial commit

--- src/main/java/fpt/fis/Controller/HistoryContrller.java
+++ src/main/java/fpt/fis/Controller/HistoryContrller.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins ="http://localhost:4200",allowedHeaders = "*")
 @RequestMapping(value = "/history")
 public class HistoryContrller {
     @Autowired
--- src/main/java/fpt/fis/Controller/NguoiThanController.java
+++ src/main/java/fpt/fis/Controller/NguoiThanController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+@CrossOrigin(origins ="http://localhost:4200",allowedHeaders = "*")
 @RequestMapping(value = "/nguoithan")
 public class NguoiThanController {
     @Autowired
